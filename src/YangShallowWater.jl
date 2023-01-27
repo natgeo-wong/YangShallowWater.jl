374ecbce72bdb7c438ec2ee4ae602bda2db2dbae
Nathanael Wong <natgeo.wong@outlook.com>
2023-01-26 23:50:52 -0500

    Defining Grids and Parameters

--- src/YangShallowWater.jl
+++ src/YangShallowWater.jl
@@ -1,5 +1,17 @@
 module YangShallowWater
 
-# Write your package code here.
+## Modules Used
+using Dates
+using FourierFlows
+using LinearAlgebra: mul!, ldiv!
+using NCDatasets
+using Printf
+import Base: eltype, show
+
+
+
+## Including other files in the module
+
+# include()
 
 end
---
+++ src/setup.jl
@@ -0,0 +1,47 @@
+struct YSWParams{FT<:Real} <: AbstractParams
+    c  :: FT # Gravity Wave speed
+    τd :: FT # Linear Damping Timescale
+    τc :: FT # Convective Damping Timescale
+    τl :: FT # WTG relaxation timescale
+    ϕ0 :: FT # Large-scale geopotential
+    ϕc :: FT # Convection-triggering geopotential
+    rc :: FT # Convection radius
+    nc :: FT # Number DENSITY of convective events
+end
+
+function GenerateGrid(
+	FT = Float64;
+	nx :: Int,
+    ny :: Int = 1,
+    Lx :: FT,
+    Ly :: FT = 0
+)
+
+    if isone(ny) || iszero(Ly)
+        return OneDGrid(;
+            nx=nx, Lx=Lx, x0=Lx/(2*nx)
+        )
+    else
+        return TwoDGrid(;
+            nx=nx, Lx=Lx, x0=Lx/(2*nx),
+            ny=ny, Ly=Ly, y0=Ly/(2*ny)
+        )
+    end
+
+end
+
+function DefineParams(
+	FT = Float64;
+	c  :: FT = 20.,     # units in m s**-1
+    τd :: FT = 0.6,     # units in hours
+    τc :: FT = 1.,      # units in days
+    τl :: FT = 1.,      # units in days
+    ϕ0 :: FT = 0,       # units in m**2 s**-2
+    ϕc :: FT = c^2,     # units in m**2 s**-2
+    rc :: FT = 10.,     # units in km
+    nc :: FT = 4.e-10,  # units in m**-2 s**-1
+)
+
+    return YSWParams{FT}(c,τd*3600,τc*86400,τl*86400,ϕ0,ϕc,rc*1000,nc)
+
+end
