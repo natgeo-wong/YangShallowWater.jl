94fa7e9bbff6616085e10c0574c61ba9c8c14c15
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-01-29 07:12:48 +1100

    can't type kwargs based on arg

--- src/setup.jl
+++ src/setup.jl
@@ -13,18 +13,20 @@ function GenerateGrid(
 	FT = Float64;
 	nx :: Int,
     ny :: Int = 1,
-    Lx :: FT,
-    Ly :: FT = 0
+    Lx,
+    Ly = 0
 )
 
     if isone(ny) || iszero(Ly)
         return OneDGrid(;
-            nx=nx, Lx=Lx, x0=Lx/(2nx)
+            nx, Lx, x0=Lx/(2nx),
+            T=FT
         )
     else
         return TwoDGrid(;
             nx, Lx, x0=Lx/(2nx),
-            ny, Ly, y0=Ly/(2ny)
+            ny, Ly, y0=Ly/(2ny),
+            T=FT
         )
     end
 
@@ -32,12 +34,12 @@ end
 
 DefineParams(
 	FT = Float64;
-	c  :: FT = 20.,     # units in m s**-1
-    τd :: FT = 0.6,     # units in hours
-    τc :: FT = 1.,      # units in days
-    τl :: FT = 1.,      # units in days
-    ϕ0 :: FT = 0,       # units in m**2 s**-2
-    ϕc :: FT = c^2,     # units in m**2 s**-2
-    rc :: FT = 10.,     # units in km
-    nc :: FT = 4.e-10,  # units in m**-2 s**-1
+	c  = 20.,     # units in m s**-1
+    τd = 0.6,     # units in hours
+    τc = 1.,      # units in days
+    τl = 1.,      # units in days
+    ϕ0 = 0,       # units in m**2 s**-2
+    ϕc = c^2,     # units in m**2 s**-2
+    rc = 10.,     # units in km
+    nc = 4.e-10,  # units in m**-2 s**-1
 ) = YSWParams{FT}(c, τd*3600, τc*86400, τl*86400, ϕ0, ϕc, rc*1000, nc)
