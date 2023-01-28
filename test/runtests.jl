deba9727627782ddbec8ba64da54a147752a8a37
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-01-29 07:20:03 +1100

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 @testset "YangShallowWater.jl" begin
     nx, Lx = 10, 2.0
     grid = GenerateGrid(; nx, Lx)
-    @test_throws "type OneDGrid has no field a" grid.Krsq
+    @test_throws "type OneDGrid has no field Krsq" grid.Krsq
 
     nx, Lx = 8, 2.0
     ny, Ly = 12, 3.0
