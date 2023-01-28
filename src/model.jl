3543c78ab11d4339346f42369fca9810ce103ae7
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-01-29 06:33:30 +1100

    fixes some dimensions and wavenumbers

--- src/model.jl
+++ src/model.jl
@@ -1,5 +1,4 @@
 
-
 struct Vars{Aphys, Atrans} <: AbstractVars
       u :: Aphys
       v :: Aphys
@@ -15,7 +14,7 @@ function Vars(::Dev, grid) where Dev
 
     T = eltype(grid)
     @devzeros Dev T (grid.nx, grid.ny) u v ϕ Fc
-    @devzeros Dev Complex{T} (grid.nk, grid.nl) uh vh ϕh Fch
+    @devzeros Dev Complex{T} (grid.nkr, grid.nl) uh vh ϕh Fch
 
     return Vars(u, v, ϕ, Fc, uh, vh, ηh, Fch)
 
@@ -23,14 +22,15 @@ end
 
 function calcN!(N, sol, t, clock, vars, params, grid)
 
-    @. vars.uh  = sol[:,:,1]
-    @. vars.vh  = sol[:,:,2]
-    @. vars.ϕh  = sol[:,:,3]
-    @. vars.Fch = sol[:,:,4]
+    @. vars.uh  = sol[:, :, 1]
+    @. vars.vh  = sol[:, :, 2]
+    @. vars.ϕh  = sol[:, :, 3]
+    @. vars.Fch = sol[:, :, 4]
 
-    @. N[:, 1] = - im * grid.nk * vars.ϕh - vars.uh / τd  # - ∂ϕ/∂x - u/τd
-    @. N[:, 2] = - im * grid.nl * vars.ϕh - vars.uh / τd  # - ∂ϕ/∂y - v/τd
-    @. N[:, 3] = - im * (grid.nk * vars.uh + grid.nl * vars.vh) + vars.Fch
+    @. N[:, :, 1] = - im * grid.kr * vars.ϕh - vars.uh / τd                     # - ∂ϕ/∂x - u/τd
+    @. N[:, :, 2] = - im * grid.l  * vars.ϕh - vars.uh / τd                     # - ∂ϕ/∂y - v/τd
+    @. N[:, :, 3] = - im * (grid.kr * vars.uh + grid.l * vars.vh) + vars.Fch    # - ∂u/∂x - ∂v/∂y - Fc
+    @. N[:, :, 4] = 0                                                           # 0 for now
 
     dealias!(N, grid)
 
@@ -41,12 +41,13 @@ end
 function Equation(dev, params, grid)
 
     T = eltype(grid)
-    L = zeros(dev, T, (grid.nkr, 3))
-    D = @. - params.ν * grid.kr^(2*params.nν)
+    L = zeros(dev, T, (grid.nkr, grid.l, 3))
+    D = @. - params.ν * grid.Krsq^params.nν
 
-    L[:, 1] .= D # for u equation
-    L[:, 2] .= D # for v equation
-    L[:, 3] .= D # for η equation
+    # for u, v, φ
+    for j in 1:3
+        L[:, :, j] .= D
+    end
 
     return FourierFlows.Equation(L, calcN!, grid)
 
@@ -56,19 +57,22 @@ function updatevars!(prob)
     
     vars, grid, sol = prob.vars, prob.grid, prob.sol
 
-    @. vars.uh = sol[:,:,1]
-    @. vars.vh = sol[:,:,2]
-    @. vars.ηh = sol[:,:,3]
+    @. vars.uh = sol[:, :, 1]
+    @. vars.vh = sol[:, :, 2]
+    @. vars.ϕh = sol[:, :, 3]
+    @. vars.Fch = sol[:, :, 4]
 
-    ldiv!(vars.u, grid.rfftplan, deepcopy(sol[:, 1])) # use deepcopy() because irfft destroys its input
-    ldiv!(vars.v, grid.rfftplan, deepcopy(sol[:, 2])) # use deepcopy() because irfft destroys its input
-    ldiv!(vars.η, grid.rfftplan, deepcopy(sol[:, 3])) # use deepcopy() because irfft destroys its input
+    # use deepcopy() below because irfft destroys its input
+    ldiv!(vars.u, grid.rfftplan, deepcopy(sol[:, :, 1]))
+    ldiv!(vars.v, grid.rfftplan, deepcopy(sol[:, :, 2]))
+    ldiv!(vars.ϕ, grid.rfftplan, deepcopy(sol[:, :, 3]))
+    ldiv!(vars.Fc, grid.rfftplan, deepcopy(sol[:, :, 4]))
 
     return nothing
 
 end
 
-function set_uvη!(prob, u0, v0, η0)
+function set_uvη!(prob, u0, v0, ϕ0, Fc0)
 
     vars, grid, sol = prob.vars, prob.grid, prob.sol
 
@@ -76,11 +80,13 @@ function set_uvη!(prob, u0, v0, η0)
 
     mul!(vars.uh, grid.rfftplan, A(u0))
     mul!(vars.vh, grid.rfftplan, A(v0))
-    mul!(vars.ηh, grid.rfftplan, A(η0))
+    mul!(vars.ϕh, grid.rfftplan, A(ϕ0))
+    mul!(vars.Fch, grid.rfftplan, A(Fc0))
 
-    @. sol[:, 1] = vars.uh
-    @. sol[:, 2] = vars.vh
-    @. sol[:, 3] = vars.ηh
+    @. sol[:, :, 1] = vars.uh
+    @. sol[:, :, 2] = vars.vh
+    @. sol[:, :, 3] = vars.ϕh
+    @. sol[:, :, 4] = vars.Fh
 
     updatevars!(prob)
 
