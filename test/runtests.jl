587bbfce4a0947b89e05a0c179fe89c8b0703cdd
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-01-29 07:13:26 +1100

    a first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using YangShallowWater
 using Test
 
 @testset "YangShallowWater.jl" begin
-    # Write your tests here.
+    nx, Lx = 10, 2.0
+    grid = GenerateGrid(; nx, Lx)
+    @test_throws "type OneDGrid has no field a" grid.Krsq
+
+    nx, Lx = 8, 2.0
+    ny, Ly = 12, 3.0
+    grid = GenerateGrid(; nx, ny, Lx, Ly)
+    @test size(grid.Ksq) == (nx, ny)
+    @test size(grid.Krsq) == (Int(nx/2+1), ny)
 end
