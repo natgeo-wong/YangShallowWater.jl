cbcb5385b2af74b44a0c4f0972e554d138432a04
Nathanael Wong <natgeo.wong@outlook.com>
2023-01-30 12:38:56 -0500

    Specify data directory in Model Structure

--- src/model.jl
+++ src/model.jl
@@ -2,37 +2,42 @@ struct Model1DSpectral{FT} <: AbstractModel
     Grid       :: OneDGrid
     Parameters :: YSWParams
     Variables  :: SpectralVars
+    Directory  :: String
 end
 
 struct Model1DFiniteDiff{FT} <: AbstractModel
     Grid       :: OneDGrid
     Parameters :: YSWParams
     Variables  :: FiniteDiffVars
+    Directory  :: String
 end
 
 struct Model2DSpectral{FT} <: AbstractModel
     Grid       :: TwoDGrid
     Parameters :: YSWParams
     Variables  :: SpectralVars
+    Directory  :: String
 end
 
 struct Model2DFiniteDiff{FT} <: AbstractModel
     Grid       :: TwoDGrid
     Parameters :: YSWParams
     Variables  :: FiniteDiffVars
+    Directory  :: String
 end
 
 function CreateModel(
     G :: OneDGrid,
     P :: YSWParams;
     spectral :: Bool = true,
+    path :: String = homedir()
 )
 
     T = eltype(G)
 
     if spectral
-          return Model1DSpectral{T}(G,P,SpectralVars(G))
-    else; return Model1DFiniteDiff{T}(G,P,FiniteDiffVars(G))
+          return Model1DSpectral{T}(G,P,SpectralVars(G),path)
+    else; return Model1DFiniteDiff{T}(G,P,FiniteDiffVars(G),path)
     end
 
 end
@@ -41,13 +46,14 @@ function CreateModel(
     G :: TwoDGrid,
     P :: YSWParams;
     spectral :: Bool = true,
+    path :: String = homedir()
 )
 
     T = eltype(G)
 
     if spectral
-          return Model2DSpectral{T}(G,P,SpectralVars(G))
-    else; return Model2DFiniteDiff{T}(G,P,FiniteDiffVars(G))
+          return Model2DSpectral{T}(G,P,SpectralVars(G),path)
+    else; return Model2DFiniteDiff{T}(G,P,FiniteDiffVars(G),path)
     end
 
 end
