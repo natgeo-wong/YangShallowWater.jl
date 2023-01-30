d617a3b38924d830f38e80ea2cb1d78b2129168f
Nathanael Wong <natgeo.wong@outlook.com>
2023-01-30 05:27:33 -0500

    Separating out grid definition

---
+++ src/grid.jl
@@ -0,0 +1,22 @@
+function GenerateGrid(
+	FT = Float64,
+	nx :: Int,
+    ny :: Int,
+    Lx :: Real,
+    Ly :: Real
+)
+
+    if isone(ny) || iszero(Ly)
+        return OneDGrid(;
+            nx, Lx, x0 = Lx/(2nx),
+            T = FT
+        )
+    else
+        return TwoDGrid(;
+            nx, Lx, x0 = Lx/(2nx),
+            ny, Ly, y0 = Ly/(2ny),
+            T = FT
+        )
+    end
+
+end
