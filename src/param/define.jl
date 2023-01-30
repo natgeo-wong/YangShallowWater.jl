42976b528ddea00af76b227fce42649f2e567247
Nathanael Wong <natgeo.wong@outlook.com>
2023-01-30 11:37:18 -0500

    Accidentally swapped τd with τc

--- src/param/define.jl
+++ src/param/define.jl
@@ -30,13 +30,13 @@ function DefineParams(
     G :: AbstractGrid;
     c :: Real = 20,      # units in m s⁻¹
     f :: Real = 0,       # units in s⁻¹
-   τd :: Real = 0.6,     # units in hours
+   τd :: Real = 1,       # units in days
     ν :: Real = 0,
    nν :: Int  = 1,
-   τc :: Real = 1,       # units in days
+   τc :: Real = 1,       # units in hours
    ϕc :: Real = c^2,     # units in m² s²
    rc :: Real = 10,      # units in km
-   Sc :: Real = 4.e-10,  # units in m**-2 s**-1
+   Sc :: Real = 5.e-14, # units in m**-2 s**-1
    τl :: Real = 1,       # units in days
    ϕ0 :: Real = 0,       # units in m² s²
    Fl :: Real = 0,
@@ -49,12 +49,12 @@ function DefineParams(
 
    if convection || ϕforcing
        P = DefineParams(
-           G,c,f,τd*3600,ν,nν,
-           τc*86400,ϕc,rc*1000,Sc,
-           τl*86400,ϕ0,wtg,Fl,
+           G,c,f,τd*86400,ν,nν,
+           τc*3600,ϕc,rc*1000,Sc,
+           τl*86400,ϕ0,Fl,wtg,
            convectionfunction!,ϕfunction!
        )
-   else; P = SimpleParams{eltype(G)}(c,f,τd*3600,ν,nν)
+   else; P = SimpleParams{eltype(G)}(c,f,τd*86400,ν,nν)
    end
 
 end
